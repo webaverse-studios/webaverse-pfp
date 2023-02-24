fa4a0ae88dc457bfa806a71e60844001b10878ff
soulofmischief <30357883+soulofmischief@users.noreply.github.com>
2023-02-24 03:06:17 -0800

    Fix typo

--- ui/bars/Navbar.tsx
+++ ui/bars/Navbar.tsx
@@ -37,7 +37,7 @@ const NavBar = () => {
           variant="filled"
           className="hover:motion-safe:animate-pulse-low"
         >
-          Conntect Wallet
+          Connect Wallet
         </Button>
       </div>
     </nav>
