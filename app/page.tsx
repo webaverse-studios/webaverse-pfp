b7ef5d35a4100e597253503fa67116cf6228471b
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2023-03-01 19:52:45 +0800

    Update styles and degen image, hide counter and mint button when the user can not mint

--- app/page.tsx
+++ app/page.tsx
@@ -7,28 +7,30 @@ import { Toaster } from 'react-hot-toast';
 
 import { Button } from '@webaverse-studios/uikit';
 
-import degen from '@/public/images/img_degen.png';
+import degen from '@/public/images/img_degen_new.png';
 import SocialBar from '@/ui/bars/SocialBar';
 import MintDialog from '@/ui/dialog/MintDialog';
-import {AppContext} from '@/ui/hooks/AccountProvider';
-import {whiteList} from '@/ui/hooks/whitelist';
+import { AppContext } from '@/ui/hooks/AccountProvider';
+import { whiteList } from '@/ui/hooks/whitelist';
 import ConnectMintButton from '@/ui/bars/ConnectMintButton';
 
 export default function Page() {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(!open);
-  const { whitelist, setWhitelist} = useContext(AppContext);
-  
+  const { whitelist, setWhitelist } = useContext(AppContext);
+
   useEffect(() => {
-    setWhitelist(whiteList)
-  }, [whiteList])
+    setWhitelist(whiteList);
+  }, [whiteList]);
 
   return (
     <>
-      <main className="relative mx-auto mt-4 flex flex-col-reverse justify-center text-gray-100 sm:mt-0 lg:-mt-[(var(--total-height))] lg:h-full lg:flex-row lg:justify-between">
+      <main className="verPositionToScreen relative mx-auto flex flex-col-reverse justify-center text-gray-100 sm:mt-0 lg:-mt-[(var(--total-height))] lg:h-full lg:flex-row lg:justify-between">
         <div className="flex flex-col justify-center rounded-sm text-center lg:max-w-md lg:text-left xl:max-w-lg">
           <h1 className="text-5xl font-bold leading-none duration-1000 motion-safe:animate-pulse-slow sm:text-6xl">
-            Degens<br/>Of The Street
+            Degens
+            <br />
+            Of The Street
           </h1>
           <p className="mt-6 mb-8 text-lg sm:mb-12">
             We are the Degens. Join us! The future of the open metaverse is in our hands! Let&apos;s
@@ -39,13 +41,13 @@ export default function Page() {
           <SocialBar />
         </div>
         <Image
-          priority
-          src={degen}
-          alt="degen"
-          width={800}
-          height={750}
-          className="degen-img right-0 bottom-0 mb-8 self-center object-contain opacity-[95%] shadow-xl sm:w-[450px] lg:mb-0 lg:w-[600px] xl:w-[650px] 2xl:absolute 2xl:w-[850px]"
-        />
+            priority
+            src={degen}
+            alt="degen"
+            width={800}
+            height={750}
+            className="degen-img right-0 bottom-0 mb-8 self-center object-contain opacity-[95%] shadow-xl sm:w-[450px] lg:mb-0 lg:w-[600px] xl:w-[650px] 2xl:absolute 2xl:w-[850px]"
+          />
         <MintDialog open={open} handleOpen={handleOpen} />
         {/* This displays toast */}
         <Toaster />
