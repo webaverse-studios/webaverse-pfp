d80ab74c4eeaa2efe44e767334dda96a019e2ff8
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2023-03-02 18:05:40 +0800

    Add webpack configs for utf-8-validate and bufferutil to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false, // Recommended for the `pages` directory, default in `app`.
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   experimental: {
     // Required:
-    appDir: false,
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
   },
 };
 
