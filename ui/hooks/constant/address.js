35cbcf9289f387a5ba41b5f31891e5b46aab774d
Hayk <55106546+mavisakalyan@users.noreply.github.com>
2023-03-04 22:02:30 +0800

    Update contract address

--- ui/hooks/constant/address.js
+++ ui/hooks/constant/address.js
@@ -1,4 +1,4 @@
 export const chainId = "0x1"
 export const epsAddress = "0x888888888888660F286A7C06cfa3407d09af44B2";
 export const passAddress = "0x2Fb4e2c6951dA92CdaC4AB1Fb9BDeea4bc941555";
-export const pfpAddress = "0xc898950c3fB910A5F9b4290dCe2D6af5F7D0E2A1";
+export const pfpAddress = "0x6e05d18E3CAbbbf377877f4d56108161083EeC02";
